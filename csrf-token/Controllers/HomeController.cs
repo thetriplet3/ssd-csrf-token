e5c569dab934181f35f801ff6f0c409887ce7d76
thetriplet3 <thetriplet3@gmail.com>
2018-07-30 01:33:03 +0530

    Added session check for redirection

--- csrf-token/Controllers/HomeController.cs
+++ csrf-token/Controllers/HomeController.cs
@@ -5,14 +5,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using csrf_token.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace csrf_token.Controllers
 {
     public class HomeController : Controller
     {
+
+        const string sessionKey = "SID";
+
         public IActionResult Index()
         {
-            return View();
+            string sessionValue = HttpContext.Session.GetString(sessionKey);
+            if (string.IsNullOrEmpty(sessionValue))
+            {
+                return RedirectToAction("Index", "Login", new { area = "" });
+            }
+            else
+            {
+                return View();
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
