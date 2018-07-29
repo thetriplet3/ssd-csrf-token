650680141e5fcef8f542bb8c2edf1207b3df7e43
thetriplet3 <thetriplet3@gmail.com>
2018-07-30 01:09:02 +0530

    Add project files.

---
+++ csrf-token/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace csrf_token.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
